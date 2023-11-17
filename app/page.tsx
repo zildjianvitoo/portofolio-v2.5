f19bd73ba4c765e226bc7a9dc0ddfb13b108ce05
ZildjianVito <zildjianvito07@gmail.com>
2023-11-17 13:27:55 +0700

    feat: make home page and responsive

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,100 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return <main>home</main>;
+  return (
+    <main className=" w-screen h-screen relative">
+      <section
+        className="flex items-center w-full h-full bg-cover bg-center"
+        style={{ backgroundImage: "url(/assets/main-bg.webp)" }}
+      >
+        <div className="md:px-20 ml-10 mb-[400px] md:mb-20 flex flex-col z-10 max-w-[750px] gap-5">
+          <h1 className="text-[36px] sm:text-[50px] text-white font-semibold">
+            Buat semuanya memungkinkan dengan{" "}
+            <span className="text-transparent font-bold bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
+              Web Development
+            </span>
+          </h1>
+          <p className="text-gray-200 hidden md:block ">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam,
+            accusamus.
+          </p>
+          <div className="hidden md:flex flex-col md:flex-row gap-5">
+            <Link
+              href={"/my-skills"}
+              className="relative rounded-[20px] bg-blue-500 group hover:bg-blue-600 px-5 py-3 text-lg text-white max-w-[200px]"
+            >
+              Baca selengkapnya
+            </Link>
+            <Link
+              href={"/my-projects"}
+              className="relative rounded-[20px] bg-blue-500 group hover:bg-blue-600 px-5 py-3 text-lg text-white max-w-[200px]"
+            >
+              <div className=" absolute rounded-[20px] z-[1] bg-white inset-0 opacity-0 group-hover:opacity-20" />
+              Project saya
+            </Link>
+            <Link
+              href={"/contact-me"}
+              className="relative rounded-[20px] bg-blue-500 group hover:bg-blue-600 px-5 py-3 text-lg text-white max-w-[200px]"
+            >
+              <div className=" absolute rounded-[20px] z-[1] bg-white inset-0 opacity-0 group-hover:opacity-20" />
+              Hubungi saya
+            </Link>
+          </div>
+        </div>
+      </section>
+      <div className="absolute flex bottom-10 z-20 right-5 flex-col md:hidden gap-5">
+        <Link
+          href={"/my-skills"}
+          className="rounded-[20px] text-center bg-blue-500 group hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]"
+        >
+          Baca selengkapnya
+        </Link>
+        <Link
+          href={"/my-projects"}
+          className="rounded-[20px] text-center bg-blue-500 group hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]"
+        >
+          <div className=" absolute rounded-[20px] z-[1] bg-white inset-0 opacity-0 group-hover:opacity-20" />
+          Project saya
+        </Link>
+        <Link
+          href={"/contact-me"}
+          className="rounded-[20px] text-center bg-blue-500 group hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]"
+        >
+          Hubungi saya
+        </Link>
+      </div>
+      <div className="absolute  bottom-0 right-0 z-10">
+        <Image
+          src={"/assets/horse.png"}
+          alt="kuda"
+          height={300}
+          width={300}
+          className="absolute -scale-x-100 sm:scale-x-100 right-56 top-40"
+        />
+        <Image
+          src={"/assets/cliff.webp"}
+          alt="tebing"
+          height={480}
+          width={480}
+        />
+      </div>
+      <div className="absolute bottom-0 z-[5] w-full h-auto">
+        <Image
+          src={"/assets/trees.webp"}
+          alt="pohon"
+          height={2000}
+          width={2000}
+          className="w-full h-full"
+        />
+      </div>
+      <Image
+        src={"/assets/stars.png"}
+        alt="bintang"
+        height={300}
+        width={300}
+        className="absolute top-10 left-0 z-10"
+      />
+    </main>
+  );
 }
