ca2caa0504d569d54b3d55aaaa5276db8bb95aaa
ZildjianVito <zildjianvito07@gmail.com>
2023-11-20 20:34:35 +0700

    fix: bg image for my projects page

--- app/my-projects/page.tsx
+++ app/my-projects/page.tsx
@@ -7,7 +7,7 @@ type Props = {};
 export default function MyProjects({}: Props) {
   return (
     <main
-      style={{ backgroundImage: "url(assets/mountains.jpg)" }}
+      style={{ backgroundImage: "url(/assets/mountains.jpg)" }}
       className="w-screen min-h-screen px-8 flex items-center  justify-center bg-center bg-cover  overflow-auto md:overflow-hidden"
     >
       <div className="my-24 lg:my-0  max-w-full  place-items-center md:mt-0 grid grid-cols-1 lg:grid-cols-2 gap-5 md:max-w-[90%]  relative">
