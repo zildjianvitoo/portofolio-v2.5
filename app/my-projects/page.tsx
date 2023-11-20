b8e33632c9314ae06bf938cc3ddbc6445903710d
ZildjianVito <zildjianvito07@gmail.com>
2023-11-20 20:33:50 +0700

    fix: margin on my projects page

--- app/my-projects/page.tsx
+++ app/my-projects/page.tsx
@@ -10,7 +10,7 @@ export default function MyProjects({}: Props) {
       style={{ backgroundImage: "url(assets/mountains.jpg)" }}
       className="w-screen min-h-screen px-8 flex items-center  justify-center bg-center bg-cover  overflow-auto md:overflow-hidden"
     >
-      <div className="my-24 max-w-full  place-items-center md:mt-0 grid grid-cols-1 lg:grid-cols-2 gap-5 md:max-w-[90%]  relative">
+      <div className="my-24 lg:my-0  max-w-full  place-items-center md:mt-0 grid grid-cols-1 lg:grid-cols-2 gap-5 md:max-w-[90%]  relative">
         {projects.map((project, index) => (
           <ProjectCard
             key={index}
